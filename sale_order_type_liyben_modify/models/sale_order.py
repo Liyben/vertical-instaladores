525c632518ec5fece4c4dac5bb29e221ae01dd4d
Liyben <soporte@liyben.com>
2021-02-01 18:59:45 +0100

    Update sale_order.py
    Modificación def onchange_type_id(self) para tener en cuenta que no se seleccione ningún impuesto.

--- sale_order_type_liyben_modify/models/sale_order.py
+++ sale_order_type_liyben_modify/models/sale_order.py
@@ -15,6 +15,12 @@ class SaleOrder(models.Model):
 			if order.type_id.tax_id:
 				for line in order.order_line:
 					line.tax_id = line.order_id.type_id.tax_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+			else:
+				for line in order.order_line:
+					fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
+					taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+					line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
+
 	
 	@api.multi
 	def _prepare_invoice(self):
