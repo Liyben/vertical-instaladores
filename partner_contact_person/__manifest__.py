f73c2818a348b67525238c7c5f5afed84e7904be
Liyben <soporte@liyben.com>
2021-07-13 13:08:19 +0200

    Upload partner_contact_person

---
+++ partner_contact_person/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Persona de contacto",
+    "category": "Extra Tools",
+    "summary": "Añade una persona de contacto a la compañia",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Liyben",
+    "website": "",
+    "depends": ["base_setup"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+}
---
+++ partner_contact_person/models/res_partner.py
@@ -0,0 +1,10 @@
+# © 2021 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    contact_person = fields.Char(string="Persona de contacto")
