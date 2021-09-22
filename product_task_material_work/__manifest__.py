4d48e72e7bd7d4259daf357da432e0158fd581c0
Liyben <soporte@liyben.com>
2021-09-22 12:44:25 +0200

    Upload print Albarán de Venta

--- product_task_material_work/__manifest__.py
+++ product_task_material_work/__manifest__.py
@@ -21,6 +21,7 @@ Este módulo contiene los mecanismos necesarios para manejar un producto como un
             'views/account_move_view.xml',
             'views/project_task.xml',
             'security/ir.model.access.csv',
+            'views/sale_report.xml'
             'views/report_saleorder.xml',
             'views/report_invoice.xml',],
     'qweb': [],
