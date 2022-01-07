151bd6177259537e3caf97a9850598751e5386be
Liyben <soporte@liyben.com>
2022-01-07 11:31:32 +0100

    [ADD] Action server in incomming mail server

--- lyb_avisos/models/crm_lead.py
+++ lyb_avisos/models/crm_lead.py
@@ -192,15 +192,3 @@ class CrmLead(models.Model):
 			value['stage_id'] = stage.id
 		return value
 
-
-	def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-
-		print("View id", view_id)
-		print("View Type", view_type)
-		print("Toolbar", toolbar)
-		print("Submenu",submenu)
-		result = super(CrmLead, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
-		print("RESULT", result)
-				
-		return result
-
