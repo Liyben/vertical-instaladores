cecdabf37af074179af845d5d91a29df2c9d58d7
Liyben <soporte@liyben.com>
2022-01-19 12:37:05 +0100

    [FIX]

--- merge_pt/models/sale.py
+++ merge_pt/models/sale.py
@@ -8,10 +8,11 @@ class SaleOrder(models.Model):
 	
 	_inherit='sale.order'
 
-	def merge_task_action_confirm(self):
-		self.action_confirm()
+	def action_confirm(self):
+		result = super(SaleOrder, self).action_confirm()
 		if len(self.tasks_ids) > 1:
 			self.merge_task_wizard_open()
+		return result
 
 	def merge_task_wizard_open(self):
 		return self.env["ir.actions.act_window"]._for_xml_id("merge_pt.action_view_sale_order_merge_task")
