6d0e7687cdd17bbeed1dff43231e1afc97f3250c
Liyben <soporte@liyben.com>
2022-01-20 11:34:28 +0100

    [FIX]

--- merge_pt/models/sale_order.py
+++ merge_pt/models/sale_order.py
@@ -8,11 +8,18 @@ class SaleOrder(models.Model):
 	
 	_inherit='sale.order'
 
-	def action_confirm(self):
-		result = super(SaleOrder, self).action_confirm()
+	def merge_task_action_confirm(self):
+		self.action_confirm()
 		if len(self.tasks_ids) > 1:
-			self.merge_task_wizard_open()
-		return result
+			return {'type': 'ir.actions.act_window',
+				'name': _('Combinar partes de trabajo'),
+				'res_model': 'sale.order.merge.task.wizard',
+				'target': 'new',
+				'view_id': self.env.ref('merge_pt.view_sale_order_merge_task').id,
+				'view_mode': 'form'}
 
+	"""
 	def merge_task_wizard_open(self):
 		return self.env["ir.actions.act_window"]._for_xml_id("merge_pt.action_view_sale_order_merge_task")
+	"""
+
