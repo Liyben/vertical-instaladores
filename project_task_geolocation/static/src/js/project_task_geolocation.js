fa4ef3c9c377656779a2fee4e4134bdca0abb7e6
Liyben <soporte@liyben.com>
2022-02-24 18:51:00 +0100

    [ADD]

--- project_task_geolocation/static/src/js/project_task_geolocation.js
+++ project_task_geolocation/static/src/js/project_task_geolocation.js
@@ -1,1 +1,18 @@
-console.log('Hello Javascript')
+odoo.define('project_task_geolocation.task_geolocation', function (require) {
+    "use strict";
+
+    var FormController = require('web.FormController');
+    var formController = FormController.include({
+        _onButtonClicked: function (event) {
+            if(event.data.attrs.name === "button_start_work"){
+                console.log("Start.");
+                alert("Start.");
+            }
+            if(event.data.attrs.name === "button_end_work"){
+                console.log("Stop.");
+                alert("Stop.");
+            }
+            this._super(event);
+        },
+    });
+});
