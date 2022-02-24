da1d356b19727aba25ffd02b9b43478152eca6a7
Liyben <soporte@liyben.com>
2022-02-24 18:47:38 +0100

    [FIX]

--- project_task_geolocation/__manifest__.py
+++ project_task_geolocation/__manifest__.py
@@ -5,7 +5,6 @@
     'name': 'Geolocalización en Partes de Trabajo',
     'version': '14.0.1.0.0',
     'category': 'Project',
-    'sequence': 6,
     'summary': """
         Con este módulo la geolocalización del empleado es rastreada en el inicio/fin
     """,
