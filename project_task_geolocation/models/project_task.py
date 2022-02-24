b629f8274a5accbffeec9e9176f5d9e9d8745508
Liyben <soporte@liyben.com>
2022-02-24 20:01:10 +0100

    [FIX]

--- project_task_geolocation/models/project_task.py
+++ project_task_geolocation/models/project_task.py
@@ -17,8 +17,7 @@ class ProjectTask(models.Model):
 		readonly=True
 	)
 
-	@api.model
-	def get_geolocation(self, location=False):
+	def get_current_geolocation(self, location=False):
 		if (location):
 			self.latitude = location[0]
 			self.longitude = location[1]
