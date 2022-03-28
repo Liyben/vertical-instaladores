cc9089c52ed87c1f79b0f06de7d1707e5a1e009e
Liyben <soporte@liyben.com>
2022-03-28 20:47:10 +0200

    [FIX] T1706

--- activity_from_crm_stage/models/crm_stage.py
+++ activity_from_crm_stage/models/crm_stage.py
@@ -6,7 +6,12 @@ from odoo import api, fields, models, _
 
 class Stage(models.Model):
 	_inherit = 'crm.stage'
+	@api.model
+	def _get_activity_type_id_domain(self):
+
+		crm_id = self.env.ref('crm.model_crm_stage').id
+		return ['|', ('res_model_id', '=', False), ('res_model_id', '=', crm_id)]
 
 	activity_type_id = fields.Many2one(
 		'mail.activity.type', string='Tipo de actividad',
-		domain="['|', ('res_model_id', '=', False), ('res_model_id', '=', ref('crm.model_crm_stage'))]", ondelete='restrict')
+		domain=_get_activity_type_id_domain, ondelete='restrict')
