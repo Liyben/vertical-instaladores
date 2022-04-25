049413c9938181150ad9455d2f4607d5e5557ab6
Liyben <soporte@liyben.com>
2022-04-25 14:07:19 +0200

    [ADD] T1695

--- project_task_geolocation/models/account_analytic_line.py
+++ project_task_geolocation/models/account_analytic_line.py
@@ -10,22 +10,26 @@ class AccountAnalyticLine(models.Model):
 	start_latitude = fields.Float(
 		"Start Latitude",
 		digits='Geolocalización',
-		readonly=True
+		readonly=True,
+		default=0.0,
 	)
 	start_longitude = fields.Float(
 		"Start Longitude",
 		digits='Geolocalización',
-		readonly=True
+		readonly=True,
+		default=0.0,
 	)
 	end_latitude = fields.Float(
 		"End Latitude",
 		digits='Geolocalización',
-		readonly=True
+		readonly=True,
+		default=0.0,
 	)
 	end_longitude = fields.Float(
 		"End Longitude",
 		digits='Geolocalización',
-		readonly=True
+		readonly=True,
+		default=0.0,
 	)
 
 	def button_start_geolocation(self):
