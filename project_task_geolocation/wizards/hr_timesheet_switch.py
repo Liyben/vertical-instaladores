91f7edf724d13264ac65e055ce9cd80d326c9aef
Liyben <soporte@liyben.com>
2022-04-26 13:09:28 +0200

    Update hr_timesheet_switch.py

--- project_task_geolocation/wizards/hr_timesheet_switch.py
+++ project_task_geolocation/wizards/hr_timesheet_switch.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, exceptions, _
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class HrTimesheetSwitch(models.TransientModel):
 	_inherit = "hr.timesheet.switch"
@@ -11,9 +12,9 @@ class HrTimesheetSwitch(models.TransientModel):
 	@api.model
 	def default_get(self, fields_list):
 		result = super().default_get(fields_list)
-		
-		result['start_latitude'] = result['task_id'].latitude
-		result['start_longitude'] = result['task_id'].longitude
+		_logger.debug('ID TASK %d', result['task'])
+		result['start_latitude'] = self.env["project.task"].search([("id", "=", result['task_id'])]).latitude
+		result['start_longitude'] = self.env["project.task"].search([("id", "=", result['task_id'])]).longitude
 		result['end_latitude'] = 0.0
 		result['end_longitude'] = 0.0
 		return result
