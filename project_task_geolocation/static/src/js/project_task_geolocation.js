98345d51a8ced5a7143e9e67f909105fb83a6ebc
Liyben <soporte@liyben.com>
2022-04-27 12:32:53 +0200

    [ADD] T1695

--- project_task_geolocation/static/src/js/project_task_geolocation.js
+++ project_task_geolocation/static/src/js/project_task_geolocation.js
@@ -71,4 +71,75 @@ odoo.define('project_task_geolocation.task_geolocation', function (require) {
             this._super(event);
         },
     });
+
+    var ListController = require('web.ListController');
+    var ListController = ListController.include({
+        update_task: function () {
+            var self = this;
+            var options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 60000,
+            };
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    self._get_geolocation.bind(self),
+                    self._getPositionError.bind(self),
+                    options
+                );
+            }
+        },
+        _get_geolocation: function (position) {
+            var self = this;
+            this._rpc({
+                model: "project.task",
+                method: "get_current_geolocation",
+                args: [[self.task.id], [position.coords.latitude, position.coords.longitude]],
+            }).then(function () {         
+                console.log('https://maps.google.com/?q='+ position.coords.latitude+','+ position.coords.longitude);
+                
+            });
+        },
+        _getPositionError: function (error) {
+            console.warn("ERROR(" + error.code + "): " + error.message);
+            const position = {
+                coords: {
+                    latitude: 0.0,
+                    longitude: 0.0,
+                },
+            };
+            this._get_geolocation(position);
+        },
+        _onButtonClicked: function (event) {
+            if(event.data.attrs.name === "get_check_in_geolocation"){
+                var self = this;
+                var id = event.data.record.data.id;
+                var def = this._rpc({
+                    model: 'project.task',
+                    method: 'search_read',
+                    args: [[['id', '=', id]]],
+                }).then(function (res) {
+                        self.task = res.length && res[0];
+                        console.log("RECORD ID: " + self.task.id);
+                    });
+                
+                this.update_task();
+            }
+            if(event.data.attrs.name === "get_check_out_geolocation"){
+                var self = this;
+                var id = event.data.record.data.id;
+                var def = this._rpc({
+                    model: 'project.task',
+                    method: 'search_read',
+                    args: [[['id', '=', id]]],
+                }).then(function (res) {
+                        self.task = res.length && res[0];
+                        console.log("RECORD ID: " + self.task.id);
+                    });
+                
+                this.update_task();
+            }
+            this._super(event);
+        },
+    });
 });
