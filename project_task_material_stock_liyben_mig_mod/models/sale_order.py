d627aed726cfd18a8dba866da663192b9b8cfe89
Liyben <soporte@liyben.com>
2022-05-19 11:29:04 +0200

    [ADD]

---
+++ project_task_material_stock_liyben_mig_mod/models/sale_order.py
@@ -0,0 +1,12 @@
+# © 2022 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models, exceptions, _
+
+class SaleOrder(models.Model):
+
+	_inherit='sale.order'
+
+	def _get_purchase_orders(self):
+		return self.env['purchase.order'].search(['|', ('group_id', 'in', self.task_ids.procurement_group_id.id), ('id', 'in', self.order_line.purchase_line_ids.order_id)])
+		
