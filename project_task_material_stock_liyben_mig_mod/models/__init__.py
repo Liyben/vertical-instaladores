cf72a1eb552178a98aaa4169ccdedc376a890e9b
Liyben <soporte@liyben.com>
2022-05-20 12:08:06 +0200

    [TEST]

--- project_task_material_stock_liyben_mig_mod/models/__init__.py
+++ project_task_material_stock_liyben_mig_mod/models/__init__.py
@@ -4,4 +4,4 @@ from . import project
 from . import project_task
 from . import account_analytic_line
 from . import stock_move
-#from . import sale_order
+from . import sale_order
--- project_task_material_stock_liyben_mig_mod/models/sale_order.py
+++ project_task_material_stock_liyben_mig_mod/models/sale_order.py
@@ -3,10 +3,18 @@
 
 from odoo import api, fields, models, exceptions, _
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
 class SaleOrder(models.Model):
 
 	_inherit='sale.order'
 
 	def _get_purchase_orders(self):
-		return self.env['purchase.order'].search([('group_id', 'in', self.tasks_ids.procurement_group_id.id)])
+		result = super(SaleOrder, self)._get_purchase_orders()
+		_logger.debug('RESULT OF SUPER %s', result)
+		""" tasks = self.mappped('tasks_ids')
+		if len(tasks) >= 1:
+			return self.env['purchase.order'].search([('group_id', 'in', self.tasks_ids.procurement_group_id.id)]) """
 		
