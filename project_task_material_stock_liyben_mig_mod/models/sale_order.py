c5fc1c73f3a11bdba7d4e45db40b6c47e716dc59
Liyben <soporte@liyben.com>
2022-05-20 12:38:28 +0200

    Update sale_order.py

--- project_task_material_stock_liyben_mig_mod/models/sale_order.py
+++ project_task_material_stock_liyben_mig_mod/models/sale_order.py
@@ -12,10 +12,13 @@ class SaleOrder(models.Model):
 	_inherit='sale.order'
 
 	def _get_purchase_orders(self):
+		purchases = []
 		result = super(SaleOrder, self)._get_purchase_orders()
-		_logger.debug('\n\n\n\nRESULT OF SUPER %s\n\n\n\n', result)
-		return result
-		""" tasks = self.mappped('tasks_ids')
+		tasks = self.mappped('tasks_ids')
 		if len(tasks) >= 1:
-			return self.env['purchase.order'].search([('group_id', 'in', self.tasks_ids.procurement_group_id.id)]) """
+			purchases = self.env['purchase.order'].search([('group_id', 'in', tasks.procurement_group_id.id)])
+		
+		
+		_logger.debug('\n\n\n\nRESULT OF SUPER %s\n\n\n\n', purchases)
+		return result
 		
