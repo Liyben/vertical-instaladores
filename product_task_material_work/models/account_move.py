04b0db1b1fcfa253d6fff6c894a360981510d30f
Liyben <soporte@liyben.com>
2022-07-15 12:57:36 +0200

    Update account_move.py

--- product_task_material_work/models/account_move.py
+++ product_task_material_work/models/account_move.py
@@ -3,6 +3,8 @@
 
 from odoo import api, fields, models, exceptions, _
 from odoo.addons import decimal_precision as dp
+from collections import defaultdict
+from odoo.tools.misc import formatLang
 
 class AccountMove(models.Model):
 
@@ -184,8 +186,59 @@ class AccountMoveLine(models.Model):
 	#Balancea las lineas de la factura asociada
 	@api.onchange('price_subtotal','price_total')
 	def _recompute_balance(self):
-		for invoice in self.move_id:
-			invoice._onchange_invoice_line_ids()
+		for move in self:
+
+			# Not working on something else than invoices.
+			if not move.is_invoice(include_receipts=True):
+				move.amount_by_group = []
+				continue
+
+			balance_multiplicator = -1 if move.is_inbound() else 1
+
+			tax_lines = move.line_ids.filtered('tax_line_id')
+			base_lines = move.line_ids.filtered('tax_ids')
+
+			tax_group_mapping = defaultdict(lambda: {
+				'base_lines': set(),
+				'base_amount': 0.0,
+				'tax_amount': 0.0,
+			})
+
+			# Compute base amounts.
+			for base_line in base_lines:
+				base_amount = balance_multiplicator * (base_line.amount_currency if base_line.currency_id else base_line.balance)
+
+				for tax in base_line.tax_ids.flatten_taxes_hierarchy():
+
+					if base_line.tax_line_id.tax_group_id == tax.tax_group_id:
+						continue
+
+					tax_group_vals = tax_group_mapping[tax.tax_group_id]
+					if base_line not in tax_group_vals['base_lines']:
+						tax_group_vals['base_amount'] += base_amount
+						tax_group_vals['base_lines'].add(base_line)
+
+			# Compute tax amounts.
+			for tax_line in tax_lines:
+				tax_amount = balance_multiplicator * (tax_line.amount_currency if tax_line.currency_id else tax_line.balance)
+				tax_group_vals = tax_group_mapping[tax_line.tax_line_id.tax_group_id]
+				tax_group_vals['tax_amount'] += tax_amount
+
+			tax_groups = sorted(tax_group_mapping.keys(), key=lambda x: x.sequence)
+			amount_by_group = []
+			for tax_group in tax_groups:
+				tax_group_vals = tax_group_mapping[tax_group]
+				amount_by_group.append((
+					tax_group.name,
+					tax_group_vals['tax_amount'],
+					tax_group_vals['base_amount'],
+					formatLang(self.env, tax_group_vals['tax_amount'], currency_obj=move.currency_id),
+					formatLang(self.env, tax_group_vals['base_amount'], currency_obj=move.currency_id),
+					len(tax_group_mapping),
+					tax_group.id
+				))
+			move.amount_by_group = amount_by_group
+			
 
 class AccountMoveLineTaskWork(models.Model):
 	"""Modelo para almacenar los trabajos del producto partida en la linea de factura"""
