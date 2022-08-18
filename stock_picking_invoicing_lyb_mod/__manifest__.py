190bab932d7f3d0ced66a707958982cad74fda19
Liyben <soporte@liyben.com>
2022-08-18 18:16:43 +0200

    Update __manifest__.py

--- stock_picking_invoicing_lyb_mod/__manifest__.py
+++ stock_picking_invoicing_lyb_mod/__manifest__.py
@@ -12,7 +12,7 @@
         
         """,
     'author': 'Liyben',
-    'depends': ['stock','account','sale','stock_picking_invoicing'],
+    'depends': ['stock','account','sale','stock_picking_invoicing','purchase_discount'],
     'data': [
             'views/stock_picking.xml',
     ],
