3565c346fc6a0d5e7c8bcd7a67e24d81a955dffd
Liyben <soporte@liyben.com>
2022-08-22 12:23:01 +0200

    Update stock_invoice_onshipping.py

--- stock_picking_invoicing_lyb_mod/wizards/stock_invoice_onshipping.py
+++ stock_picking_invoicing_lyb_mod/wizards/stock_invoice_onshipping.py
@@ -11,7 +11,7 @@ class StockInvoiceOnshipping(models.TransientModel):
 	
 	def _load_pickings(self):
 		pickings = super()._load_pickings()
-		if 'assigned' in pickings.mapped("state"):
+		if pickings.mapped("state") in ("draft", "cancel", "assigned", "waiting", "confirmed"):
 			raise UserError(_("¡Todos los albaranes deben estar en estado Hecho!"))
 		pickings.set_sale_to_invoiced()
 		pickings.set_purchase_to_invoiced()
