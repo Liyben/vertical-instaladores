870f56e07fabe83546f048a5b7a9c8f0bfa4189f
Liyben <soporte@liyben.com>
2022-08-24 16:46:03 +0200

    [ADD][T1858]

--- supplier_delivery_note_number/models/stock.py
+++ supplier_delivery_note_number/models/stock.py
@@ -1,6 +1,7 @@
 # © 2022 Liyben
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import string
 from odoo import fields, models
 
 
@@ -9,3 +10,10 @@ class StockPicking(models.Model):
 
     
     supplier_pick_number = fields.Char(string='Nº. albarán proveedor')
+    use_supplier_pick_number = fields.Boolean(related='picking_type_id.use_supplier_pick_number',
+        readonly=True)
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    use_supplier_pick_number = fields.Boolean(string='Tiene número de albarán')
