85ed809a316c25c3337980a76c3cba9613c3d936
Liyben <soporte@liyben.com>
2022-10-05 13:33:01 +0200

    [ADD] [T2005] print_options_to_false_account_move_line

---
+++ print_options_to_false_account_move_line/__manifest__.py
@@ -0,0 +1,24 @@
+# © 2022 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Opciones de impresion en linea de factura',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'summary': 'Trabajos y Materiales en producto',
+    'description': """
+Este módulo al crear una factura desde el presupuesto, las opciones de impresión de cada linea de factura hacen que tomen el valor False.
+    """,
+    'author': 'Liyben',
+    'depends': ['product_task_material_work'],
+    'data': [],
+    'qweb': [],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+}
