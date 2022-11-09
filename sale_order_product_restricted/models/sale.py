1c0c35e3ffb5d36ed07f2b09058ba63dca6ae1e6
Liyben <soporte@liyben.com>
2022-11-09 13:31:40 +0100

    [ADD] [T2073]  sale_order_product_restricted

---
+++ sale_order_product_restricted/models/sale.py
@@ -0,0 +1,20 @@
+# © 2022 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api, _
+
+class SaleOrder(models.Model):
+
+	_inherit='sale.order'
+
+
+	# % desperdicio
+	can_be_confirmed = fields.Boolean(compute = '_compute_can_be_confirmed')
+
+
+	@api.depends('order_line','order_line.product_id')
+	def _compute_total_secciones(self):
+		for record in self:
+			record.can_be_confirmed = False
+			for line in record.order_line:
+				record.can_be_confirmed = record.can_be_confirmed or line.product_id.is_template
