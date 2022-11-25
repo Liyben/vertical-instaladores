a26e6ead22522056253e9f61ce2f4ae861b80d81
Liyben <soporte@liyben.com>
2022-11-25 14:19:47 +0100

    [FIX][T2098] traducciones

--- stock_picking_add_2_report_valued/__manifest__.py
+++ stock_picking_add_2_report_valued/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Informes de albaran valorizados',
+    'name': 'Informes de albaran valorado',
     'category': 'Warehouse Management',
-    'summary': 'Añade dos nuevos informes a los albaranes. Estos informes están valorizados. Módulo basado en stock_picking_report_valued de la OCA.',
+    'summary': 'Añade dos nuevos informes a los albaranes. Estos informes están valorado. Módulo basado en stock_picking_report_valued de la OCA.',
     'website': 'https://liyben.com/',
     'version': '14.0.1.0.0',
     'license': 'AGPL-3',
