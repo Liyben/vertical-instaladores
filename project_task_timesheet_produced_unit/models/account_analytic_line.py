80855b7fa6b4abedefde7b03e048499e489f7fd1
Liyben <soporte@liyben.com>
2022-11-30 14:33:45 +0100

    Update account_analytic_line.py

--- project_task_timesheet_produced_unit/models/account_analytic_line.py
+++ project_task_timesheet_produced_unit/models/account_analytic_line.py
@@ -12,7 +12,7 @@ class AccountAnalyticLine(models.Model):
 	#Dominio para el campo mano de obra
 	@api.model
 	def _get_product_produced_unit_id_domain(self):
-		ids = self.env['project.task.material'].search().mapped('product_id').ids
+		ids = self.env['project.task.material'].search([]).mapped('product_id').ids
 		return [('id', 'in', ids), ('cost_produced_unit', '>', 0)]
 
 	produced_unit = fields.Float(
