1f666571e9e9c0832f6d2e547fc379c1a906c675
Liyben <soporte@liyben.com>
2022-11-30 11:32:08 +0100

    Update account_analytic_line.py

--- project_task_timesheet_produced_unit/models/account_analytic_line.py
+++ project_task_timesheet_produced_unit/models/account_analytic_line.py
@@ -10,8 +10,8 @@ class AccountAnalyticLine(models.Model):
 	@api.model
 	def _get_product_produced_unit_id(self):
 		ids = self.task_id.material_ids.search([]).mapped('material_id').ids
-		domain = [ ('id', 'in', ids) ]
-		return self.env['product.product'].search(domain, limit=1)
+		domain = [ ('id', 'in', ids), ('cost_produced_unit', '>', 0.0) ]
+		return self.env['product.product'].search(domain)
 		
 	produced_unit = fields.Float(
 		"Unidades producidas",
