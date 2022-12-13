eeb92858d0f991db617bd8cc1eb86adc4bdeff08
Liyben <soporte@liyben.com>
2022-12-13 13:27:25 +0100

    [TEST]

--- lyb_crm_sat/models/crm_lead.py
+++ lyb_crm_sat/models/crm_lead.py
@@ -7,6 +7,9 @@ from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _, SUPERUSER_ID
 from lxml import etree
 
+import logging
+_logger = logging.getLogger(__name__)
+
 class CrmLead(models.Model):
 	_inherit = 'crm.lead'
 
@@ -119,8 +122,27 @@ class CrmLead(models.Model):
 			
 		return super(CrmLead,self).copy(default=default)
 
+	@api.depends('user_id', 'type')
+	def _compute_team_id(self):
+		""" When changing the user, also set a team_id or restrict team id
+		to the ones user_id is member of. """
+		for lead in self:
+			# setting user as void should not trigger a new team computation
+			if not lead.user_id:
+				continue
+			user = lead.user_id
+			if lead.team_id and user in lead.team_id.member_ids | lead.team_id.user_id:
+				continue
+			team_domain = [('use_leads', '=', True)] if lead.type == 'lead' else [('use_opportunities', '=', True)]
+			team = self.env['crm.team']._get_default_team_id(user_id=user.id, domain=team_domain)
+			user_salesteam_id = self.env['res.users'].browse(user.id).sale_team_id.id
+			if not user_salesteam_id:
+				_logger.debug(".........................NOT TEAM_ID\n")
+				lead.team_id = False
+			else:	
+				lead.team_id = team.id
 
-class CrmTeam(models.Model):
+""" class CrmTeam(models.Model):
 	_inherit = 'crm.team'
 
 	def _get_default_team_id(self, user_id=None, domain=None):
@@ -135,4 +157,4 @@ class CrmTeam(models.Model):
 		], limit=1)
 		if not team and 'default_team_id' in self.env.context:
 			team = self.env['crm.team'].browse(self.env.context.get('default_team_id'))
-		return team or self.env['crm.team'].search(domain or [], limit=1)
+		return team or self.env['crm.team'].search(domain or [], limit=1) """
