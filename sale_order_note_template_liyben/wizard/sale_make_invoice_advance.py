a147eae9c2de042664e54554814a23b1f4227c1b
Liyben <soporte@liyben.com>
2023-03-31 12:54:17 +0200

    Update sale_make_invoice_advance.py

--- sale_order_note_template_liyben/wizard/sale_make_invoice_advance.py
+++ sale_order_note_template_liyben/wizard/sale_make_invoice_advance.py
@@ -7,7 +7,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
 	_inherit = "sale.advance.payment.inv"
 
 	def _prepare_invoice_values(self, order, name, amount, so_line):
-		res = super()._prepare_invoice_values((order, name, amount, so_line))
+		res = super()._prepare_invoice_values(order, name, amount, so_line)
 		if order.terms_template_id:
 			res['narration'] = ''
 		
