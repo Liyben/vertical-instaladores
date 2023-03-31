4a01409c7062b7de139a1469d3ae97d5d93d645a
Liyben <soporte@liyben.com>
2023-03-31 12:53:15 +0200

    Update sale_make_invoice_advance.py

--- sale_order_note_template_liyben/wizard/sale_make_invoice_advance.py
+++ sale_order_note_template_liyben/wizard/sale_make_invoice_advance.py
@@ -6,8 +6,8 @@ from odoo import api, fields, models, exceptions, _
 class SaleAdvancePaymentInv(models.TransientModel):
 	_inherit = "sale.advance.payment.inv"
 
-	def _prepare_invoice_values(self, order, so_line):
-		res = super()._prepare_invoice_values(order, so_line)
+	def _prepare_invoice_values(self, order, name, amount, so_line):
+		res = super()._prepare_invoice_values((order, name, amount, so_line))
 		if order.terms_template_id:
 			res['narration'] = ''
 		
