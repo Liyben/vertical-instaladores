f06ba118f51f5484e046502f0e171b9b7c1cd38e
Liyben <soporte@liyben.com>
2023-05-12 12:59:48 +0200

    Update calendar_event.py

--- url_description_calendar_event/models/calendar_event.py
+++ url_description_calendar_event/models/calendar_event.py
@@ -14,8 +14,9 @@ class Meeting(models.Model):
         url = self.env['ir.config_parameter'].get_param('web.base.url')
         if self.res_model and self.res_id:
             url = url + '/web#id=' + str(self.res_id) + '&model=' + self.res_model + '&view_type=form'
-            #self.description = self.description + url
-            _logger.debug('%s\n', self.description)
+            desc = self.description + '\n' + url
+            _logger.debug('%s\n', desc)
             _logger.debug('%s\n', url)
+            self.description = desc
         return res
     
