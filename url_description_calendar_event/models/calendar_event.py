1361345c3f51a9f3fccf275830533c34c420aa55
Liyben <soporte@liyben.com>
2023-05-12 13:48:49 +0200

    Update calendar_event.py

--- url_description_calendar_event/models/calendar_event.py
+++ url_description_calendar_event/models/calendar_event.py
@@ -10,13 +10,14 @@ class Meeting(models.Model):
     _inherit = 'calendar.event'
 
     def write(self, values):
-        res = super(Meeting, self).write(values)
         url = self.env['ir.config_parameter'].get_param('web.base.url')
-        if self.res_model and self.res_id:
-            url = url + '/web#id=' + str(self.res_id) + '&model=' + self.res_model + '&view_type=form'
+        res_id = values.get('res_id')
+        res_model = values.get('res_model')
+        if res_model and res_id:
+            url = url + '/web#id=' + str(res_id) + '&model=' + res_model + '&view_type=form'
             desc = self.description + '\n' + url
             _logger.debug('%s\n', desc)
             _logger.debug('%s\n', url)
-            self.description = url
-        return res
+            values['description'] = url
+        return super(Meeting, self).write(values)
     
