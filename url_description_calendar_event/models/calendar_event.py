9497f226f5f479b33932e10ced9281e468d82888
Liyben <soporte@liyben.com>
2023-05-13 11:03:07 +0200

    Update calendar_event.py

--- url_description_calendar_event/models/calendar_event.py
+++ url_description_calendar_event/models/calendar_event.py
@@ -3,22 +3,30 @@
 
 from odoo import api, fields, models, _
 
-import json
 import logging
 _logger = logging.getLogger(__name__)
 
 class Meeting(models.Model):
-    _inherit = 'calendar.event'
+	_inherit = 'calendar.event'
 
-    def write(self, values):
-        url = self.env['ir.config_parameter'].get_param('web.base.url')
-        res_id = self.res_id
-        res_model = self.res_model
-        if res_model and res_id and 'description' in values:
-            action = self.env[self.res_model].browse(self.res_id).get_formview_action()
-            _logger.debug('\n\n\n%s\n\n',json.dumps(action, indent = 4))
-            url = url + '/web#id=' + str(res_id) + '&model=' + res_model + '&view_type=form'
-            desc = values.get('description') + '\n' + url
-            values['description'] = desc
-        return super(Meeting, self).write(values)
-    
+	def write(self, values):
+		url = self.env['ir.config_parameter'].get_param('web.base.url')
+		res_id = self.res_id
+		res_model = self.res_model
+		if res_model and res_id and 'description' in values:
+			url = url + '/web#id=' + str(res_id) + '&model=' + res_model + '&view_type=form'
+			desc = values.get('description') + '\n' + url
+			values['description'] = desc
+		return super(Meeting, self).write(values)
+	
+	@api.model_create_multi
+	def create(self, vals_list):
+		res = super(Meeting, self).create(vals_list)
+		url = self.env['ir.config_parameter'].get_param('web.base.url')
+		res_id = self.res_id
+		res_model = self.res_model
+		if res_model and res_id:
+			url = url + '/web#id=' + str(res_id) + '&model=' + res_model + '&view_type=form'
+			desc = self.description + '\n' + url
+			self.description = desc
+		return res
