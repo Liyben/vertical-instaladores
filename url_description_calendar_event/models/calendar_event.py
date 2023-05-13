4925df543e094306cdff61cf1f428ba0b6b127f7
Liyben <soporte@liyben.com>
2023-05-13 11:45:43 +0200

    Update calendar_event.py

--- url_description_calendar_event/models/calendar_event.py
+++ url_description_calendar_event/models/calendar_event.py
@@ -3,6 +3,10 @@
 
 from odoo import api, fields, models, _
 
+import json
+import pytz
+from odoo import tools
+
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -19,26 +23,27 @@ class Meeting(models.Model):
 			values['description'] = desc
 		return super(Meeting, self).write(values)
 	
-	@api.model_create_multi
-	def create(self, vals_list):
-		self = self.with_context(is_calendar_event_new=True)
-		defaults = self.default_get(['activity_ids', 'res_model_id', 'res_id', 'res_model', 'description'])
-		meeting_activity_type = self.env['mail.activity.type'].search([('category', '=', 'meeting')], limit=1)
-		# get list of models ids and filter out None values directly
-		model_ids = list(filter(None, {values.get('res_model_id', defaults.get('res_model_id')) for values in vals_list}))
-		model_name = defaults.get('res_model')
-		valid_activity_model_ids = model_name and self.env[model_name].sudo().browse(model_ids).filtered(lambda m: 'activity_ids' in m).ids or []
-		if meeting_activity_type and not defaults.get('activity_ids'):
-			for values in vals_list:
-				# created from calendar: try to create an activity on the related record
-				if values.get('activity_ids'):
-					continue
-				res_model_id = values.get('res_model_id', defaults.get('res_model_id'))
-				values['res_id'] = res_id = values.get('res_id') or defaults.get('res_id')
-				url = self.env['ir.config_parameter'].get_param('web.base.url')
-				if res_model_id and res_id:
-					url = url + '/web#id=' + str(res_id) + '&model=' + res_model_id + '&view_type=form'
-					desc = values.get('description') or defaults.get('description')
-					_logger.debug('\n\n\n%s\n\n',desc + url)
-					values['description'] = desc + '\n' + url
-		return super(Meeting, self).create(vals_list)
+	def _sync_activities(self, fields):
+		# update activities
+		_logger.debug('%s\n',json.dumps(fields, indent=4))
+		for event in self:
+			if event.activity_ids:
+				activity_values = {}
+				if 'name' in fields:
+					activity_values['summary'] = event.name
+				if 'description' in fields:
+					activity_values['note'] = event.description and tools.plaintext2html(event.description)
+				if 'start' in fields:
+					# self.start is a datetime UTC *only when the event is not allday*
+					# activty.date_deadline is a date (No TZ, but should represent the day in which the user's TZ is)
+					# See 72254129dbaeae58d0a2055cba4e4a82cde495b7 for the same issue, but elsewhere
+					deadline = event.start
+					user_tz = self.env.context.get('tz')
+					if user_tz and not event.allday:
+						deadline = pytz.UTC.localize(deadline)
+						deadline = deadline.astimezone(pytz.timezone(user_tz))
+					activity_values['date_deadline'] = deadline.date()
+				if 'user_id' in fields:
+					activity_values['user_id'] = event.user_id.id
+				if activity_values.keys():
+					event.activity_ids.write(activity_values)
