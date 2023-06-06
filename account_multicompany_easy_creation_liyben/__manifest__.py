42e554744b6c2bb4a077708bc52bbd5e8c2046f7
Liyben <soporte@liyben.com>
2023-06-06 12:57:04 +0200

    [ADD] [T0379] account_multicompany_easy_creation_liyben

---
+++ account_multicompany_easy_creation_liyben/__manifest__.py
@@ -0,0 +1,27 @@
+# © 2023 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Creación rápida de empresa desde Contabilidad',
+    'category': "Accounting",
+    'summary': '',
+    'website': 'https://liyben.com/',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': """
+        
+            
+        """,
+    'author': 'Liyben',
+    'depends': ['account_multicompany_easy_creation',],
+    'data': [
+        'views/menuitem.xml',
+    ],
+    'qweb': [],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+}
