5d4bb012b90f46b56a9cf3a06a605514a0ae6875
Liyben <soporte@liyben.com>
2023-07-17 14:07:06 +0200

    [ADD][T0500]

---
+++ hide_footer_login_screen/__init__.py
@@ -0,0 +1,2 @@
+# © 2023 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
---
+++ hide_footer_login_screen/__manifest__.py
@@ -0,0 +1,27 @@
+# © 2023 Liyben
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Oculta el pie de la ventana de registro',
+    'category': "Web",
+    'summary': '',
+    'website': 'https://liyben.com/',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': """
+        
+            
+        """,
+    'author': 'Liyben',
+    'depends': ['web',],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'qweb': [],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+}
