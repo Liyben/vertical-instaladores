22f2e8da60546351dd418254437fa2926895d7af
Liyben <soporte@liyben.com>
2024-02-08 12:17:42 +0100

    [FIX]

--- partner_risk_on_invoice/models/account_invoice.py
+++ partner_risk_on_invoice/models/account_invoice.py
@@ -10,8 +10,8 @@ class AccountMove(models.Model):
 	@api.depends('partner_id', 'partner_id.name', 'partner_id.insurance_credit_limit')
 	def _compute_credit_limit(self):
 		for invoice in self:
-			
-			invoice.partner_risk_credit_limit = invoice.partner_id.name + " - (Riesgo: " + str(invoice.partner_id.insurance_credit_limit) + " €)"
+			insurance_credit_limit = "{:,.2f}".format(invoice.partner_id.insurance_credit_limit).replace(",", "@").replace(".", ",").replace("@", ".")
+			invoice.partner_risk_credit_limit = invoice.partner_id.name + " - (Riesgo: " + insurance_credit_limit + " €)"
 			invoice.insurance_credit_limit = invoice.partner_id.insurance_credit_limit
 
 	
