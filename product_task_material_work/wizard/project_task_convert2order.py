a56a022c241a7d0ae12f703ee12d690c662d839e
Liyben <soporte@liyben.com>
2024-06-14 12:58:37 +0200

    [ADD][T1345]order line values

--- product_task_material_work/wizard/project_task_convert2order.py
+++ product_task_material_work/wizard/project_task_convert2order.py
@@ -22,7 +22,7 @@ class ProjectTaskConvert2Order(models.TransientModel):
 		comodel_name="project.task", string="Tarea", 
 	)
 	product_id = fields.Many2one(
-		comodel_name="product.product", string="Producto", required=True, 
+		comodel_name="product.product", string="Producto", required=True, domain="[('service_tracking', 'in', ['task_global_project', 'task_in_project'])]"
 	)
 
 	def action_project_task_to(self):
@@ -31,13 +31,55 @@ class ProjectTaskConvert2Order(models.TransientModel):
 		if not self.task_id.partner_id:
 			raise ValidationError(_('No existe ningún cliente asociado al PT.'))
 		
+		order_line = []
+		if self.task_id:
+			material_list = []
+			work_list = []
+			auto_create_task = False
+			product = self.product_id
+			if product:
+				auto_create_task = (product.service_tracking == 'task_global_project') or (product.service_tracking == 'task_in_project')
+			
+			if auto_create_task:
+				for work in self.task_id.material_ids:
+					work_list.append((0,0, {
+						'name' : work.name,
+						'work_id': work.work_id.id,
+						'sale_price_unit' : work.sale_price_unit,
+						'cost_price_unit' : work.cost_price_unit,
+						'hours' : work.hours,
+						}))
+				
+				for material in self.task_id.material_ids:
+					material_list.append((0,0, {
+						'material_id' : material.material_id.id,
+						'name' : material.name,
+						'sale_price_unit' : material.sale_price_unit,
+						'cost_price_unit' : material.cost_price_unit,
+						'quantity' : material.quantity,
+						}))
+
+				order_line.append((0,0,{'product_id': self.product_id.id,
+					'name': self.task_id.work_to_do,
+					'product_uom_qty':1.0,
+					'product_uom': self.product_id.product_uom.id,
+					#'price_unit': line.price_unit,
+					#'purchase_price': line.cost_unit,
+					#'tax_id':[(6, 0, line.tax_id.ids)],
+					#'task_works_ids' : work_list,
+					#'task_materials_ids' : material_list,
+					#'auto_create_task' : auto_create_task, 
+					'task_id': self.task_id.id, 
+					}))
+
 		action['context'] = {
-			'search_default_partner_id': self.task_id.partner_id.id,
+			#'search_default_partner_id': self.task_id.partner_id.id,
 			'default_partner_id': self.task_id.partner_id.id,
 			'default_user_id': self.env.user.id,
 			'default_company_id': self.task_id.company_id.id or self.env.company.id,
 			#'default_tag_ids': [(6, 0, self.tag_ids.ids)]
 		}
+
 		if self.task_id.project_id:
 			action['context']['default_project_id'] = self.task_id.project_id.id
 		if self.task_id.analytic_account_id:
@@ -46,5 +88,7 @@ class ProjectTaskConvert2Order(models.TransientModel):
 			action['context']['default_opportunity_id'] = self.task_id.oppor_id.id
 		if self.task_id.oppor_id.team_id:
 			action['context']['default_team_id'] = self.task_id.oppor_id.team_id.id
-		
+		if order_line:
+			action['context']['default_order_line'] = order_line
+
 		return action
