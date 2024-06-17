51229d5c6edaf5c23bfeb18ab0ef50f76e343e9f
Liyben <soporte@liyben.com>
2024-06-17 10:27:14 +0200

    [FIX][T1345] change logic: create order

--- product_task_material_work/wizard/project_task_convert2order.py
+++ product_task_material_work/wizard/project_task_convert2order.py
@@ -25,74 +25,32 @@ class ProjectTaskConvert2Order(models.TransientModel):
 		comodel_name="product.product", string="Producto", required=True, domain="[('service_tracking', 'in', ['task_global_project', 'task_in_project'])]"
 	)
 
+	def _get_sale_order_data(self):
+		self.ensure_one()
+		res = {
+			"partner_id": self.task_id.partner_id.id,
+			"analytic_account_id": self.task_id.analytic_account_id.id or False,
+			"project_id": self.task_id.project_id.id or False,
+			"opportunity_id": self.task_id.oppor_id.id or False,
+			"team_id": self.task_id.oppor_id.team_id.id or False,
+			"user_id": self.env.user.id,
+			"company_id": self.task_id.company_id.id or self.env.company.id,
+		}
+		return res
+	
 	def action_project_task_to(self):
+
 		if not self.task_id.partner_id:
 			raise ValidationError(_('No existe ningún cliente asociado al PT.'))
 		
-		action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations_with_onboarding")
+		action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations")
 		action['views'] = [(self.env.ref('sale.view_order_form').id, 'form')]
 		
-		
-		order_line = []
-		if self.task_id:
-			material_list = []
-			work_list = []
-			auto_create_task = False
-			product = self.product_id
-			if product:
-				auto_create_task = (product.service_tracking == 'task_global_project') or (product.service_tracking == 'task_in_project')
-			
-			if auto_create_task:
-				for work in self.task_id.material_ids:
-					work_list.append((0,0, {
-						'name' : work.name,
-						'work_id': work.work_id.id,
-						'sale_price_unit' : work.sale_price_unit,
-						'cost_price_unit' : work.cost_price_unit,
-						'hours' : work.hours,
-						}))
-				
-				for material in self.task_id.material_ids:
-					material_list.append((0,0, {
-						'material_id' : material.material_id.id,
-						'name' : material.name,
-						'sale_price_unit' : material.sale_price_unit,
-						'cost_price_unit' : material.cost_price_unit,
-						'quantity' : material.quantity,
-						}))
-
-				order_line.append((0,0,{'product_id': self.product_id.id,
-					'name': self.task_id.work_to_do,
-					'product_uom_qty':1.0,
-					#'product_uom': self.product_id.product_uom.id,
-					#'price_unit': line.price_unit,
-					#'purchase_price': line.cost_unit,
-					#'tax_id':[(6, 0, line.tax_id.ids)],
-					#'task_works_ids' : work_list,
-					#'task_materials_ids' : material_list,
-					#'auto_create_task' : auto_create_task, 
-					'task_id': self.task_id.id, 
-					}))
-		else:
-			order_line = False
-
-		action['context'] = {
-			#'search_default_partner_id': self.task_id.partner_id.id,
-			'default_partner_id': self.task_id.partner_id.id,
-			'default_user_id': self.env.user.id,
-			'default_company_id': self.task_id.company_id.id or self.env.company.id,
-			#'default_tag_ids': [(6, 0, self.tag_ids.ids)]
-		}
-
-		if self.task_id.project_id:
-			action['context']['default_project_id'] = self.task_id.project_id.id
-		if self.task_id.analytic_account_id:
-			action['context']['default_analytic_account_id'] = self.task_id.analytic_account_id.id
-		if self.task_id.oppor_id:
-			action['context']['default_opportunity_id'] = self.task_id.oppor_id.id
-		if self.task_id.oppor_id.team_id:
-			action['context']['default_team_id'] = self.task_id.oppor_id.team_id.id
-		if order_line:
-			action['context']['default_order_line'] = order_line
+		order_model = self.env["sale.order"].sudo()
+		sale_order_data = self._get_sale_order_data()
+		sale_order = order_model.create(sale_order_data)
+		sale_order.onchange_partner_id()
+	
+		action["res_id"] = sale_order.id
 
 		return action
