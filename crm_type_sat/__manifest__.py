cfa05eefc618c2fe1373b7b5c4095610c180718e
Liyben <soporte@liyben.com>
2024-12-05 12:20:53 +0100

    Update __manifest__.py

--- crm_type_sat/__manifest__.py
+++ crm_type_sat/__manifest__.py
@@ -6,7 +6,7 @@
     'category': "Customer Relationship Management",
     'summary': 'Añade los avisos',
     'website': 'https://liyben.com/',
-    'version': '14.0.1.0.0',
+    'version': '17.0.1.0.0',
     'license': 'AGPL-3',
     'description': """
         
