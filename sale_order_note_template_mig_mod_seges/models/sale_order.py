44335c3db7cb3c9e6cd82271e0f1faee0fc6d0b1
Liyben <soporte@liyben.com>
2024-12-10 16:41:07 +0100

    [FIX]

--- sale_order_note_template_mig_mod_seges/models/sale_order.py
+++ sale_order_note_template_mig_mod_seges/models/sale_order.py
@@ -12,10 +12,7 @@ class SaleOrder(models.Model):
     terms_template_id = fields.Many2one(
         "sale.terms_template",
         string="Terms and conditions template",
-        readonly=True,
-        states={"draft": [("readonly", False)]},
     )
-    note = fields.Html(readonly=True, states={"draft": [("readonly", False)]})
 
     @api.onchange("terms_template_id")
     def _onchange_terms_template_id(self):
