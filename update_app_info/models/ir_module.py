e1087de63f9e447a2b70800cf786cec34d644e6a
Liyben <soporte@liyben.com>
2024-12-24 14:11:13 +0100

    Update ir_module.py

--- update_app_info/models/ir_module.py
+++ update_app_info/models/ir_module.py
@@ -7,6 +7,7 @@ import json
 from odoo import api, fields, models, modules, tools, _
 from odoo.service.common import exp_version
 from odoo.addons.base_import_module.models.ir_module import APPS_URL
+from odoo.release import major_version
 
 _logger = logging.getLogger(__name__)
 class Module(models.Model):
@@ -22,7 +23,7 @@ class Module(models.Model):
         _logger.debug("LIST: %s\n", str(modules_to_update_dict))
         return True
     
-    def check_app_store_updates(self, modules):
+    def check_app_store_updates(self, modules,fields):
         """Check for updates in the app store for the given modules.
 
         Args:
@@ -44,21 +45,32 @@ class Module(models.Model):
         """
         # odoo apps server
         #url = f"{APPS_URL}/apps/embed/update"
-        url = f"{APPS_URL}/loempia/listdatamodules"
-        payload = {
+        #url = f"{APPS_URL}/loempia/listdatamodules"
+        """ payload = {
             "jsonrpc": "2.0",
             "method": "call",
             "params": {
                 "modules": modules,
                 "version": exp_version(),
             },
+        } """
+        payload = {
+            'params': {
+                'series': major_version,
+                'module_fields': fields,
+                'module_type': 'official',
+                'module_name': self.env.context.get('module_name'),
+                'domain': None,
+                'limit': None,
+                'offset': None,
+            }
         }
-        #resp = self._call_apps(json.dumps(payload))
-        #resp.raise_for_status()
-        #modules_list = resp.json().get('result', [])
-        response = requests.Session().post(url, json=payload).json()
-        _logger.debug("RESPONSE: %s\n", str(response.get("result")))
-        return response.get("result")
+        resp = self._call_apps(json.dumps(payload))
+        resp.raise_for_status()
+        modules_list = resp.json().get('result', [])
+        #response = requests.Session().post(url, json=payload).json()
+        _logger.debug("RESPONSE: %s\n", str(modules_list))
+        return modules_list
 
     def get_modules_need_update(self):
         """Find installed modules and get a dictionary of modules that need an update.
@@ -80,20 +92,21 @@ class Module(models.Model):
         """
         # find the installed modules and prepare the dictionary
         # for the query on the server
-        installed_modules = self.search_read(
-            [("state", "=", "installed")],
-            fields=[
+        fields = [
                 "id",
                 "name",
                 "installed_version",
                 "latest_version",
                 "state",
                 "display_name",
-            ],
+            ]
+        installed_modules = self.search_read(
+            [("state", "=", "installed")],
+            fields=fields,
         )
         modules_dict = {record["name"]: record for record in installed_modules}
         _logger.debug("MODULES DICT: %s\n", str(modules_dict))
         # Check for updates
-        modules_to_update_dict = self.check_app_store_updates(modules_dict)
+        modules_to_update_dict = self.check_app_store_updates(modules_dict, fields)
 
         return modules_to_update_dict
