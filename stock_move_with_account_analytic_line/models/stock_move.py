ea1e219087f34d15178b9fc062f1d29baafd15c5
Seges <cuentas@seges.es>
2025-02-11 19:19:54 +0100

    Update stock_move.py

--- stock_move_with_account_analytic_line/models/stock_move.py
+++ stock_move_with_account_analytic_line/models/stock_move.py
@@ -18,7 +18,7 @@ class StockMove(models.Model):
                 and rec.state == "done"
             ):
                 analytic_line_vals = self._prepare_analytic_line(rec)
-        _logger.debug("AL: %s\n", str(analytic_line_vals))
+                _logger.debug("AL: %s\n", str(analytic_line_vals))
         #if analytic_line_vals:    
         #    self.env['account.analytic.line'].create(analytic_line_vals)
         return res
