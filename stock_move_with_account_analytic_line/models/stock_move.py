0ac326fe5b27f6f22ce72714892c09efea9fb261
Seges <cuentas@seges.es>
2025-02-11 19:06:22 +0100

    [TEST]

--- stock_move_with_account_analytic_line/models/stock_move.py
+++ stock_move_with_account_analytic_line/models/stock_move.py
@@ -18,28 +18,30 @@ class StockMove(models.Model):
                 and rec.product_id.categ_id.property_valuation == "only_analytic"
                 and rec.state == "done"
             ):
-                analytic_line_vals += self._prepare_analytic_line(rec)
+                analytic_line_vals += rec._prepare_analytic_line()
         _logger.debug("AL: %s\n", str(analytic_line_vals))
-        if analytic_line_vals:    
-            self.env['account.analytic.line'].create(analytic_line_vals)
+        #if analytic_line_vals:    
+        #    self.env['account.analytic.line'].create(analytic_line_vals)
         return res
     
     def _prepare_analytic_line(self, move):
-        amount = 0.0 - move.product_id.standard_price 
-        if move.location_id and move.location_id.usage == "customer":
-            amount *= -1.0
-        return {
-            'name': "{} - {}".format(move.reference, move.product_id.name),
-            'date': fields.date.today(),
-            'account_id': move.analytic_account_id.id,
-            'group_id': move.analytic_account_id.group_id.id,
-            'unit_amount': move.quantity_done,
-            'product_id': move.product_id and move.product_id.id or False,
-            'product_uom_id': move.product_uom and move.product_uom.id or False,
-            'amount': amount,
-            'general_account_id': self.env.ref('l10n_es.1_account_common_300').id or False,
-            'ref': "{} - {}".format(move.reference, move.product_id.name),
-            'user_id': self._uid,
-            'partner_id': move.partner_id.id,
-            'company_id': move.analytic_account_id.company_id.id or move.company_id.id,
-        }
+        result = []
+        for move in self:
+            amount = 0.0 - move.product_id.standard_price 
+            if move.location_id and move.location_id.usage == "customer":
+                amount *= -1.0
+            result.append({
+                'name': "{} - {}".format(move.reference, move.product_id.name),
+                'date': fields.date.today(),
+                'account_id': move.analytic_account_id.id,
+                'group_id': move.analytic_account_id.group_id.id,
+                'unit_amount': move.quantity_done,
+                'product_id': move.product_id and move.product_id.id or False,
+                'product_uom_id': move.product_uom and move.product_uom.id or False,
+                'amount': amount,
+                'general_account_id': self.env.ref('l10n_es.1_account_common_300').id or False,
+                'ref': "{} - {}".format(move.reference, move.product_id.name),
+                'user_id': self._uid,
+                'partner_id': move.partner_id.id,
+                'company_id': move.analytic_account_id.company_id.id or move.company_id.id,
+            })
