a5329a9aa488dc50dcec6c6d18bb98ff87a08ee4
Seges <cuentas@seges.es>
2025-03-31 19:23:30 +0200

    [TEST]

--- project_stock/models/project_task.py
+++ project_stock/models/project_task.py
@@ -182,6 +182,7 @@ class ProjectTask(models.Model):
                     and x.location_id == loc and x.location_dest_id == loc_dest and x.picking_type_id == pick_type
                 )
             )
+            _logger.debug("ANALYTIC DISTRIBUTION MOVES UPDATE: %s\n", str(moves))
             moves.update(
                 {
                     "analytic_distribution": item.analytic_distribution,
@@ -287,6 +288,7 @@ class ProjectTask(models.Model):
                 self._update_moves_group_id()
 
                 if self.stock_analytic_distribution:
+                    _logger.debug("ANALYTIC DISTRIBUTION\n")
                     self._update_analytic_distribution_info()
                 # Avoid permissions error if the user does not have access to stock.
                 #_logger.debug("ACTION ASSIGN\n")
