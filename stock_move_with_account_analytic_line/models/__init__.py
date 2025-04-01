127ec9cc3117cdc46c97de38101a44998dab5224
Seges <cuentas@seges.es>
2025-04-01 12:36:20 +0200

    Update __init__.py

--- stock_move_with_account_analytic_line/models/__init__.py
+++ stock_move_with_account_analytic_line/models/__init__.py
@@ -1,6 +1,6 @@
 # © 2025 Seges
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import account_analytic_line
+#from . import account_analytic_line
 from . import product_category
 from . import stock_move
