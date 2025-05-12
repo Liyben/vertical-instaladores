5e1017469a73a2bc40ec7d12a559e531d32a0038
Seges <cuentas@seges.es>
2025-05-12 18:43:47 +0200

    [FIX]

--- project_task_geolocation/models/project_task.py
+++ project_task_geolocation/models/project_task.py
@@ -13,44 +13,44 @@ class ProjectTask(models.Model):
 	view_button_stop = fields.Boolean(string='Ver boton comienzo', compute='_compute_view_button_start', store=True)
 
 	def _compute_view_button_start(self):
-		self.ensure_one()
-		if self.env.user:
-			employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
-			if employee:
-				now = fields.Datetime.now()
-				attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
-				if attendance:
-					aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
-					if aal:
-						return False
+		for record in self:
+			if self.env.user:
+				employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
+				if employee:
+					now = fields.Datetime.now()
+					attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
+					if attendance:
+						aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
+						if aal:
+							return False
+						else:
+							return True
 					else:
-						return True
+						return False
 				else:
 					return False
 			else:
 				return False
-		else:
-			return False
 		
 	def _compute_view_button_start(self):
-		self.ensure_one()
-		if self.env.user:
-			employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
-			if employee:
-				now = fields.Datetime.now()
-				attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
-				if attendance:
-					aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id), ('task_id', '=', self.id)])
-					if aal:
-						return True
+		for record in self:
+			if self.env.user:
+				employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
+				if employee:
+					now = fields.Datetime.now()
+					attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
+					if attendance:
+						aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id), ('task_id', '=', self.id)])
+						if aal:
+							return True
+						else:
+							return False
 					else:
 						return False
 				else:
 					return False
 			else:
 				return False
-		else:
-			return False
 
 	""" def get_start_geolocation(self):
 		self.ensure_one()
