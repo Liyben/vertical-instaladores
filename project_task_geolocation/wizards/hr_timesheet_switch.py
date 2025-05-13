9bc82457a56bcdbbf347fdf4a181111104e03265
Seges <cuentas@seges.es>
2025-05-13 13:48:05 +0200

    [FIX]

--- project_task_geolocation/wizards/hr_timesheet_switch.py
+++ project_task_geolocation/wizards/hr_timesheet_switch.py
@@ -8,6 +8,31 @@ _logger = logging.getLogger(__name__)
 class HrTimesheetSwitch(models.TransientModel):
     _inherit = "hr.timesheet.switch"
 
+    def _get_user_attendance(self):
+        employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
+        if employee:
+            now = fields.Datetime.now()
+            attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
+            if attendance:
+                aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
+                if not aal:
+                    return attendance
+        
+        return False
+
+    def _prepare_copy_worker_values(self, record):
+        """Return the values that will be overwritten in new timesheet entry."""
+        attendance = self._get_user_attendance()
+        return {
+            "name": record.name,
+            "date_time": record.date_time,
+            "date_time_end": record.date_time_end,
+            "project_id": record.project_id.id,
+            "task_id": record.task_id.id,
+            "unit_amount": 0,
+            "attendance_id": attendance.id or False,
+        }
+
     def action_switch(self):
         """Stop old timer, start new one."""
         self.ensure_one()
@@ -18,26 +43,17 @@ class HrTimesheetSwitch(models.TransientModel):
         ).running_timer_id.button_end_work()
         # Start new timer
         if self.analytic_line_id:
-            _logger.debug("IF\n")
-            new = self.analytic_line_id.copy(self._prepare_copy_values(self))
+            if self.env.user.has_group('project_task_geolocation.group_geolocation_worker'):
+                new = self.analytic_line_id.copy(self._prepare_copy_worker_values(self))
+            else:
+                new = self.analytic_line_id.copy(self._prepare_copy_values(self))
         else:
-            _logger.debug("ELSE\n")
             fields = self.env["account.analytic.line"]._fields.keys()
             vals = self.env["account.analytic.line"].default_get(fields)
-            vals.update(self._prepare_copy_values(self))
             if self.env.user.has_group('project_task_geolocation.group_geolocation_worker'):
-                _logger.debug("ELSE-IF\n")
-                employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
-                if employee:
-                    _logger.debug("ELSE-IF-IF\n")
-                    now = fields.Datetime.now()
-                    attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
-                    aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
-                    if attendance and not aal:
-                        _logger.debug("ELSE-IF-IF-IF\n")
-                        vals.update({
-                        "attendance_id": attendance.id,
-                        })
+                vals.update(self._prepare_copy_worker_values(self))
+            else:
+                vals.update(self._prepare_copy_values(self))
             new = self.env["account.analytic.line"].create(vals)
         # Display created timer record if requested
         if self.env.context.get("show_created_timer"):
