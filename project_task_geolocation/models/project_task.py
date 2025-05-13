55e5259c4ffd6480e07307cce2e407fb5d803c56
Seges <cuentas@seges.es>
2025-05-13 17:51:54 +0200

    [TEST]

--- project_task_geolocation/models/project_task.py
+++ project_task_geolocation/models/project_task.py
@@ -9,10 +9,10 @@ _logger = logging.getLogger(__name__)
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    view_button_start = fields.Boolean(string='Ver boton comienzo', compute='_compute_view_button_start', store=True)
+    view_button_start = fields.Boolean(string='Ver boton comienzo', compute='_compute_view_button_start', store=True, precompute=True,)
     view_button_stop = fields.Boolean(string='Ver boton comienzo', compute='_compute_view_button_stop', store=True)
 
-    @api.depends("user_ids.employee_ids.last_attendance_id")
+
     def _compute_show_time_control(self):
         """Decide when to show time controls."""
         result = super()._compute_show_time_control()
@@ -24,9 +24,13 @@ class ProjectTask(models.Model):
                         now = fields.Datetime.now()
                         attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id), ('check_in', '<=', now), ('check_out', '=', False)], order="check_in desc", limit=1)
                         if attendance:
-                            aal = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
-                            if aal:
-                                task.show_time_control = False
+                            aal_start = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id)])
+                            if aal_start:
+                                aal_stop = self.env['account.analytic.line'].search([('attendance_id', '=', attendance.id), ('employee_id', '=', employee.id), ('task_id', '=', task.id)])
+                                if aal_stop:
+                                    task.show_time_control = "stop"
+                                else:
+                                    task.show_time_control = False
                             else:
                                 task.show_time_control = "start"
                         else:
@@ -37,9 +41,9 @@ class ProjectTask(models.Model):
                     task.show_time_control = False
 
         return result    
-    
-     
-    """ def _compute_view_button_start(self):
+
+    api.depends("user_ids","user_ids.employee_ids.last_attendance_id",)
+    def _compute_view_button_start(self):
         for record in self:
             if self.env.user and self.env.user in record.user_ids:
                 employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id), ('company_id', '=', self.env.company.id)])
@@ -58,7 +62,7 @@ class ProjectTask(models.Model):
                     record.view_button_start = False
             else:
                 record.view_button_start = False
-        
+    """ 
     def _compute_view_button_stop(self):
         for record in self:
             if self.env.user:
