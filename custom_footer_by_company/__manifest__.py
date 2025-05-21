72fc7a8c5ed1d7c401f0dd8bfec24f7230f76837
Seges <cuentas@seges.es>
2025-05-21 12:45:30 +0200

    [FIX]

--- custom_footer_by_company/__manifest__.py
+++ custom_footer_by_company/__manifest__.py
@@ -2,20 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Pie de página personalizado',
-    'version': '',
+    'version': '17.0.1.0.0',
     'summary': """ Pie de página personalizado """,
-    'author': '',
-    'website': '',
-    'category': '',
+    'author': 'Seges',
+    'website': 'seges.es',
+    'category': 'Reporting',
     'depends': ['base', 'web'],
     "data": [
-        "security/ir.model.access.csv",
         "template/report_tamplate.xml",
         "views/res_company_views.xml",
-        "wizards/base_document_layout.xml"
+        "wizards/base_document_layout.xml",
     ],
-    
-    'application': True,
     'installable': True,
     'auto_install': False,
     'license': 'AGPL-3',
