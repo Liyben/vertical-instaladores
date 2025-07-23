f2b0050954eaed35f3317f65f452d5e3ee044c48
Seges <cuentas@seges.es>
2025-07-23 19:48:10 +0200

    [ADD][TS0374] compute discount from pricelist

--- product_task_material_work/models/sale_order_line_task_work.py
+++ product_task_material_work/models/sale_order_line_task_work.py
@@ -44,7 +44,7 @@ class SaleOrderLineTaskWork(models.Model):
     #Horas empleadas en el trabajo
     hours = fields.Float(string='Hr.')
     #Descuento aplicado al precio de la mano de obra
-    discount = fields.Float(string='Des. (%)', digits='Discount', default=0.0)
+    discount = fields.Float(string='Des. (%)', digits='Discount', store=True, precompute=True, compute='_compute_discount')
     sequence = fields.Integer()
     #Margen
     work_margin = fields.Float(string='Margen', digits='Product Price', compute='_compute_price')
@@ -74,13 +74,10 @@ class SaleOrderLineTaskWork(models.Model):
         self.ensure_one()
         self.order_line_id.ensure_one()
         self.work_id.ensure_one()
-        _logger.debug("self.work_id: %s\n",str(self.work_id))
 
         #Guardamos los precios de la ficha de mano de obra
         product_lst_price = self.work_id.list_price
         product_standard_price = self.work_id.standard_price
-        _logger.debug("product_lst_price: %s\n",str(product_lst_price))
-        _logger.debug("product_standard_price: %s\n",str(product_standard_price))
         #Actualizamos los precios de la ficha de mano de obra con los precios de la linea de mano de obra
         self.work_id.write({
             'list_price' : self.sale_price_unit,
@@ -94,29 +91,112 @@ class SaleOrderLineTaskWork(models.Model):
             uom=self.work_id.uom_id,
             date=self._get_order_date(),
         )
-        _logger.debug("pricelist_item_id: %s\n",str(pricelist_item_id))
+        
         pricelist_item = self.env['product.pricelist.item'].search([
             ('id', '=', pricelist_item_id)
         ])
-        _logger.debug("pricelist_item: %s\n",str(pricelist_item))
+        
         #Aplicamos tarifa
         price = pricelist_item._compute_price(
             product=self.work_id,
             quantity=self.hours or 1.0,
             uom=self.work_id.uom_id,
             date=self._get_order_date(),
-            #date=self.order_line_id._get_order_date(),
             currency=self.currency_id,
         )
 
-        _logger.debug("price: %s\n",str(price))
+        if self.order_line_id.order_id.pricelist_id.discount_policy == 'without_discount' or not pricelist_item:
+            
+            #Recuperamos los precios de la ficha de mano de obra previamente guardado
+            self.work_id.write({
+                'list_price' : product_lst_price,
+                'standard_price' : product_standard_price,
+                })
+            
+            return price
+        
+        base_price = pricelist_item._compute_price_before_discount(
+            product=self.work_id,
+            quantity=self.hours or 1.0,
+            uom=self.work_id.uom_id,
+            date=self._get_order_date(),
+            currency=self.currency_id,
+        )
+
         #Recuperamos los precios de la ficha de mano de obra previamente guardado
         self.work_id.write({
             'list_price' : product_lst_price,
             'standard_price' : product_standard_price,
             })
         
+        return max(base_price, price)
+    
+    #Devuelve el precio base
+    def _get_pricelist_price_before_discount(self, rec, pricelist_item):
+        rec.ensure_one()
+        rec.work_id.ensure_one()
+
+        return pricelist_item._compute_price_before_discount(
+            product=rec.work_id,
+            quantity=rec.hours or 1.0,
+            uom=rec.work_id.uom_id,
+            date=rec._get_order_date(),
+            currency=rec.currency_id,
+        )
+    
+    #Devuelve el precio dado por la tarifa
+    def _get_pricelist_price(self, rec, pricelist_item):
+        rec.ensure_one()
+        rec.work_id.ensure_one()
+
+        price = pricelist_item._compute_price(
+            product=rec.work_id,
+            quantity=rec.hours or 1.0,
+            uom=rec.work_id.uom_id,
+            date=rec._get_order_date(),
+            currency=rec.currency_id,
+        )
+
         return price
+    
+    #Calculo del descuento si se aplica tarifa
+    @api.depends('work_id')
+    def _compute_discount(self):
+        for record in self:
+            if not record.work_id:
+                record.discount = 0.0
+
+            if not (
+                record.order_line_id.order_id.pricelist_id 
+                and record.order_line_id.order_id.pricelist_id.discount_policy == 'without_discount'
+            ):  
+                continue
+            
+            record.discount = 0.0
+
+            #Obtenemos el elemento de tarifa
+            pricelist_item_id = record.order_line_id.order_id.pricelist_id._get_product_rule(
+                record.work_id,
+                quantity=record.hours or 1.0,
+                uom=record.work_id.uom_id,
+                date=record._get_order_date(),
+            )
+            
+            pricelist_item = self.env['product.pricelist.item'].search([
+                ('id', '=', pricelist_item_id)
+            ])
+
+            if not pricelist_item:
+                continue
+            
+            record = record.with_company(record.company_id)
+            pricelist_price = record._get_pricelist_price(record, pricelist_item)
+            base_price = record._get_pricelist_price_before_discount(record, pricelist_item)
+
+            if base_price != 0:
+                discount = (base_price - pricelist_price) / base_price * 100
+                if (discount > 0 and base_price > 0) or (discount < 0 and base_price < 0):
+                    record.discount = discount
 
     #Calculo de los precios de venta y coste totales por linea de los trabajos
     @api.depends('hours','sale_price_unit', 'cost_price_unit', 'discount')
@@ -126,15 +206,6 @@ class SaleOrderLineTaskWork(models.Model):
         self.work_margin = 0.0
         self.work_margin_percent = 0.0
         for record in self:
-            """ if record._check_apply_pricelist():
-                record = record.with_company(record.company_id)
-                price = record.sale_price_unit
-                if record.work_id:
-                    price = record._get_display_price()
-                record.sale_price = record.hours * (price * (1 - (record.discount / 100)))
-                record.cost_price = (record.hours * record.cost_price_unit)
-                record.work_margin = (record.hours * (price * (1 - (record.discount / 100)))) - (record.hours * record.cost_price_unit)
-            else: """
             record.sale_price = record.hours * (record.sale_price_unit * (1 - (record.discount / 100)))
             record.cost_price = (record.hours * record.cost_price_unit)
             record.work_margin = (record.hours * (record.sale_price_unit * (1 - (record.discount / 100)))) - (record.hours * record.cost_price_unit)
