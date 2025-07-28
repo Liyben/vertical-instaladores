be84de870b841f293a9f351e9e4ea1641101dd32
Seges <cuentas@seges.es>
2025-07-28 18:06:23 +0200

    [ADD][TS0508] retention on account.move

--- sale_invoice_retention/models/account_move.py
+++ sale_invoice_retention/models/account_move.py
@@ -12,8 +12,28 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    @api.depends('amount_untaxed', 'amount_total', 'percent_retention')
+    def _amount_all_retention(self):
+        for order in self:
+            order.amount_retention = order.amount_untaxed * order.percent_retention / 100
+            order.amount_to_pay = order.amount_total - order.amount_retention
+
     percent_retention = fields.Float(
         string="Retención (%)",
         digits="Retention",
         readonly=False,
+    )
+
+    amount_retention = fields.Monetary(
+        string="Retención", 
+        store=True, 
+        readonly=True,
+        compute='_amount_all_retention', 
+    )
+
+    amount_to_pay = fields.Monetary(
+        string="Total a pagar", 
+        store=True, 
+        readonly=True,
+        compute='_amount_all_retention', 
     )
