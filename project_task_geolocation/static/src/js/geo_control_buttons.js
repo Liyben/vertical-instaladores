ce7a850c7ce0a18ffe0d3eb5762ad6f467107ac9
Seges <cuentas@seges.es>
2025-12-16 10:34:43 +0100

    [FIX] catch in onGeoAction function

--- project_task_geolocation/static/src/js/geo_control_buttons.js
+++ project_task_geolocation/static/src/js/geo_control_buttons.js
@@ -47,9 +47,26 @@ export class GeoControlButtons extends Component {
                     }
 
                 } catch (e) {
-                    console.error(e);
-                    const errorMessage = e.message && e.message.data ? e.message.data.message : (e.message || e.toString());
-                    this.notification.add("Error en servidor: " + e.message.data.message, { type: "danger" });
+                    console.error("Geo Error:", e);
+                    
+                    let errorMessage = "Ha ocurrido un error inesperado.";
+
+                    // Caso 1: Error RPC detallado de Odoo (UserError, ValidationError desde Python)
+                    // La estructura suele ser e.message.data.message
+                    if (e.message && e.message.data && e.message.data.message) {
+                        errorMessage = e.message.data.message;
+                    }
+                    // Caso 2: Error estándar de Javascript o error simple de Odoo
+                    // Solo tiene e.message
+                    else if (e.message) {
+                        errorMessage = e.message;
+                    }
+                    // Caso 3: Si 'e' es solo un string u otro objeto
+                    else {
+                        errorMessage = e.toString();
+                    }
+
+                    this.notification.add("Error: " + errorMessage, { type: "danger" });
                 }
             },
             (err) => {
