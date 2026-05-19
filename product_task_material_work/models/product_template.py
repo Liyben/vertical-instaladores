2df48bb8d5ec3d7981953c7a0c23f8faa0386f53
Sergio Lenovo <sergio.cuentas@seges.es>
2026-05-19 14:46:49 +0200

    [TEST]

--- product_task_material_work/models/product_template.py
+++ product_task_material_work/models/product_template.py
@@ -156,7 +156,7 @@ class ProductTemplate(models.Model):
 		self.ensure_one()
 
 		# 2. Obtener la acción de forma segura sin disparar errores de ACL
-		action = self.env['ir.actions.act_window'].sudo()._for_xml_id('sale.product_template_action')
+		action = self.env.ref('sale.product_template_action').sudo().read()[0]
 
 		products_compound = self.env['product.template'] # Recordset vacío por defecto
 
